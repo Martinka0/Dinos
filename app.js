b175c1fb3fe7935a432998e7fc78caffc341b811
Martina <martinaklimova0@gmail.com>
2021-04-05 15:33:17 +0200

    json Dino Issue

--- app.js
+++ app.js
@@ -32,69 +32,19 @@ function Dino(species, height, weight, diet, where, when, fact, img) {
   this.fact = fact;
   this.img = img;
 };
-// Create Dino Objects
-//add a randomFact method in the Dino constructor 
-Dino.prototype.randomInfo = function (item) {
-  const info = ['weigt', 'height', 'diet', 'fact'];
-  const randomNumber = Math.floor(Math.random() * Math.floor(info.length));
-  return this[info[randomNumber]](item);
-};
-
-let dinos = [];
-fetch("./dino.json")
-  .then((res) => res.json())
-  .then((data) => {
-    dinos = data.Dinos;
-    console.log(dinos);
-  getDinos(dinos);
-  });
-function getDinos(ds) {
-  ds.map((dino) => console.log(dino.species));
+Dino.prototype.dietCompare = function(){
+  if (human.diet === this.diet){
+      return `${this.species} was a ${this.diet}. You two could share dinner.`;
+  } else if (this.diet === "carnivor"){
+      return `${this.species} was a ${this.diet}. Better run before you become the meal.`;
+  } else if (this.diet === "herbavor"){
+      return `${this.species} was a ${this.diet}. You'll have to prepare an extra salad for dinner.`;
+  } else {
+      return `${this.species} was a ${this.diet}. Time to suggest a potluck.`;
+  }
 }
-
-console.log(dino.species);
-
-
-//const human = new Human();
-function getHumanData(){
-  human={};
-  human.name = document.getElementById('name').value;
-  human.height = (document.getElementById('feet').value * 12) + document.getElementById('inches').value;
-  human.weight = document.getElementById('weight').value;
-  human.diet = document.getElementById('diet').value;
-  console.log(human);
-  return human
-}
-
-  // Use IIFE to get human data from form   
-const button = document.getElementById('btn');
-button.addEventListener("click", (event)=>{
-  event.preventDefault();
-  console.log("working!!")
-  getHumanData();
-});    
-
-   
-    //  Dino Compare Method 1
-  
-    Dino.prototype.weight = function (){
-      const humanWeight = human.weight;
-      const dinoWeight = this.weight;
-      const difference = Math.abc(this.weight - human.weight); 
-      
-      
-      if (humanWeight > dinoWeight) {
-      return `The ${this.species} weighs + ${difference} + more then ${human.name}.`;
-     
-      } else if (dinoWeight < humanWeight) {
-      return `The ${this.species} weighs + ${difference} + less then ${human.name}.`;  
-      } else {
-       return `the ${this.species} weighs the same as ${human.name}`; 
-       
-      }
-    };
-    
-    // Create Dino Compare Method 2
+ 
+// Create Dino Compare Method 2
     Dino.prototype.height = function (){
       const humanHeight = human.height;
       const dinoHeight = this.height;
@@ -111,9 +61,6 @@ button.addEventListener("click", (event)=>{
       }
     };
     
-    
-
-    
     // Create Dino Compare Method 3
 
     Dino.prototype.diet = function () {
@@ -126,24 +73,94 @@ button.addEventListener("click", (event)=>{
       }
       };
 
+  // https://knowledge.udacity.com/questions/542357
     
-   
+// Create Dino Objects
+//add a randomFact method in the Dino constructor 
+Dino.prototype.randomInfo = function (item) {
+  const info = ['weigt', 'height', 'diet', 'fact'];
+  const randomNumber = Math.floor(Math.random() * Math.floor(info.length));
+  return this[info[randomNumber]](item);
+};
 
+// let dinos = [];
+// fetch("./dino.json")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     //if (data.Dinos !== Array) {
+//       console.log(typeof data.Dinos);
+//    // }
+    
+//     dinos = data.Dinos;
+//     console.log(dinos);
+//   getDinos(dinos);
+//   });
+// function getDinos(ds) {
+  
+//   ds.map((dino) => console.log(dino));
+// };
 
-    // Generate Tiles for each Dino in Array
-    const grid = document.querySelector('grid');
+//const human = new Human();
+function getHumanData(){
+  human={};
+  human.name = document.getElementById('name').value;
+  human.height = (document.getElementById('feet').value * 12) + document.getElementById('inches').value;
+  human.weight = document.getElementById('weight').value;
+  human.diet = document.getElementById('diet').value;
+  console.log(human);
+  return human
+}
 
-    const dinoTile = document.createElement('p');
-    dinoTile.textContent = 'We hope you enjoyed the ride.';
+  // Use IIFE to get human data from form   
+const button = document.getElementById('btn');
+button.addEventListener("click", (event)=>{
+  const getDinos = async () => {
+    const fetchedData = await fetch ("./dino.json");
+    const data = await fetchedData.json();
+    return data;
+  };
+  const createDinos = async () => {
+    const dinos = await getDinos();
+    {
+  
+ //uncought referrence in promise
+ data.map((dino) => console.log(dino));
+    };
+    }; 
+  
+  createDinos();
+  event.preventDefault();
+  console.log("working!!")
+  //getHumanData();
+  //displayInfographic();
+  removeForm();
+});    
+
+// Generate Tiles for each Dino in Array
+
+//  function = displayInfographic () {
+//  let dinos = getDinos();
+//  const human = getHumanData();
+
+//  }
+//     const grid = document.querySelector('grid');
+  
+//     const cell = document.createElement('div');
+
+//     cell.className = 'grid-item';
+// // 
+//     const dinoTile = document.createElement('p');
+//     dinoTile.textContent = '?';
     
 
 
-    // Add tiles to DOM
+//     // Add tiles to DOM
 
-    grid.appendChild(dinoTile);
+//     grid.appendChild(dinoTile);
+
 
     // Remove form from screen
-
+dinoCompare = document.getElementById('dino-compare');
 function removeForm() {
   dinoCompare.style.display = "none";
 }
