3d943e38f9b50989f895d2bf69d551f2141a3e3e
Martina <martinaklimova0@gmail.com>
2021-04-20 23:55:22 +0200

    updated dino and human function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function Dino(species, height, weight, diet, where, when, fact, img) {
   this.img = img;
 };
 // Create Dino Compare Method 1
-Dino.prototype.dietCompare = function(humanDiet){
+Dino.prototype.dietCompare = function(){
   if (human.diet === this.diet){
       return `${this.species} was a ${this.diet}. You two could share dinner.`;
   } else if (this.diet === "carnivor"){
@@ -26,7 +26,7 @@ Dino.prototype.dietCompare = function(humanDiet){
 }
  
 // Create Dino Compare Method 2
-    Dino.prototype.height = function (humanHeight){
+    Dino.prototype.height = function (){
       const humanHeight = human.height;
       const dinoHeight = this.height;
       const difference = Math.abc(dinoHeight - humanHeight); 
@@ -44,7 +44,7 @@ Dino.prototype.dietCompare = function(humanDiet){
     
 // Create Dino Compare Method 3
 
-Dino.prototype.weight = function (humanWeight){
+Dino.prototype.weight = function (){
   const humanWeight = human.weight;
   const dinoWeight = this.weight;
   const difference = Math.abc(dinoWeight - humanWeight); 
@@ -74,7 +74,9 @@ Dino.prototype.randomInfo = function (item) {
 
 //const human = new Human();
 function getHumanData(){
-  human={};
+  const human = getHumanData();
+  human = {};
+ 
   human.name = document.getElementById('name').value;
   human.height = (document.getElementById('feet').value * 12 ) + document.getElementById('inches').value;
   human.weight = document.getElementById('weight').value;
@@ -85,66 +87,84 @@ function getHumanData(){
 }
 
   // Use IIFE to get human data from form   
-  (function Human() {
-    getHumanData();
 
-  })();
-
-const button = document.getElementById('btn');
-button.addEventListener("click", (event)=>{
   const getDinos = async () => {
     const fetchedData = await fetch ("./dino.json");
     const data = await fetchedData.json();
-    return data;
-  };
+    return data};
+
+const button = document.getElementById('btn');
+button.addEventListener("click", (event)=>{
+  listener.event.preventDefault();
+  
+  
+  // Move everything here, as it should be executed after the promise is resolved.
   const createDinos = async () => {
     const dinos = await getDinos();
-    {
-  
- //uncought referrence in promise resolved : https://knowledge.udacity.com/questions/542357
- dinos.Dinos.map((dino) => {
-    generateUi(dino)
-    console.log(dino);
-    
-    
+    // getHumanData already returns a human object
+    // so you don't need the IIFE above.
+    // (don't forget to declare human as const inside getHumanData)
+    const human = getHumanData();
+    // Instead of mapping through dinos.Dinos straight away,
+    // you need to add the human before doing it and create
+    // the Dino instances.
+    // And for a cleaner code, I would add that to a variable (like dinoList).
+    const dinoList = dinos.Dinos.map((dino) => {
+      return new Dino(
+        dino.species,
+        dino.height,
+        dino.weight,
+        dino.diet,
+        dino.where,
+        dino.when,
+        dino.fact
+      );
     });
-    
-    };
-    }; 
-
-
-  
+    // Add the human to the dinos array
+    dinoList.splice(4, 0, human);
+    // Since you are not storing a copy of the dinos array,
+    // you should use forEach here.
+    dinoList.forEach((dino) => {
+      // Pass human here as parameter to use it later
+      generateUi(dino, human);
+    });
+    removeForm();
+  };
   createDinos();
+});
+
+
   
-  event.preventDefault();
-  console.log("working!!") 
-  getHumanData(human);
-  //dino.splice(4, 0,human);
-  removeForm();
-});    
+     
 // Generate Tiles for each Dino in Array
 
-const generateUi=(dino)=>{
-    const grid = document.querySelector('#grid');
-    const head=document.createElement('h3');
-    const image=document.createElement('img');
-    const fact=document.createElement('p');
-    //const randomInfo=document.createElement('p');
-    head.textContent=dino.species;
-    image.src=`./images/${dino.species}.png`;
-    fact.textContent=dino.fact;
-
+  const generateUi = (dino, human) => {
+    const grid = document.querySelector("#grid");
+    const head = document.createElement("h3");
+    const image = document.createElement("img");
+    const fact = document.createElement("p");
     
-  // Add tiles to DOM
-    const newTile = document.createElement('div');
-    newTile.className = 'grid-item';
+    // If dino has a name, it is a human and we want to display it.
+    head.textContent = dino.name || dino.species;
+    
+    // Now, if dino does not have species, it is a human.
+    // Or you could add 'human' as a 'species' property to
+    // the human object.
+    image.src = `./images/${dino.species || "human"}.png`;
+    // Only show fact if the dino has one.
+    if (dino.fact) {
+      fact.textContent = dino.fact;
+    }
+    const newTile = document.createElement("div");
+    newTile.className = "grid-item";
+    // The elements should be appended to the tile
+    // and not the grid.
+    newTile.appendChild(head);
+    newTile.appendChild(image);
+    newTile.appendChild(fact);
+    // Then you append the tile to the grid.
     grid.appendChild(newTile);
-    grid.appendChild(head);
-    grid.appendChild(image);
-    grid.appendChild(fact);
-    //grid.appendChild(randomInfo);
-  }
-
+  };
 
     // Remove form from screen
 dinoCompare = document.getElementById('dino-compare');
