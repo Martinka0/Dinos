cba163be1717de617f235e3f94a90c7a337bdcef
Martina <martinaklimova0@gmail.com>
2021-04-25 22:44:58 +0200

    cleand up code

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 //Create a Dino construtor or class.
 function Dino(species, height, weight, diet, where, when, fact, img) {
   this.species = species;
@@ -12,156 +10,158 @@ function Dino(species, height, weight, diet, where, when, fact, img) {
   this.img = img;
 };
 // Create Dino Compare Method 1
-Dino.prototype.dietCompare = function(human){
-  if (human.diet === this.diet){
+Dino.prototype.dietCompare = function(human) {
+  if (human.diet === this.diet) {
       return `${this.species} was a ${this.diet}. You two could share dinner.`;
-  } else if (this.diet === "carnivor"){
+  } else if (this.diet === "carnivor") {
       return `${this.species} was a ${this.diet}. Better run before you become the meal.`;
-  } else if (this.diet === "herbavor"){
+  } else if (this.diet === "herbavor") {
       return `${this.species} was a ${this.diet}. You'll have to prepare an extra salad for dinner.`;
   } else {
       return `${this.species} was a ${this.diet}. Time to suggest a potluck.`;
   }
 };
- 
-// Create Dino Compare Method 2
-    Dino.prototype.heightCompare = function (human){
-      const humanHeight = human.height;
-      const dinoHeight = this.height;
-      const difference = Math.abs(dinoHeight - humanHeight); 
-      
-      if (humanHeight > dinoHeight) {
-      return `The ${this.species} is  ${difference} inches smaller then ${human.name}.`;
-     
-      } else if (dinoHeight > humanHeight) {
-      return `The ${this.species} is  ${difference} inches taller then ${human.name}.`;  
-      } else {
-       return `the ${this.species} height is the same as ${human.name}`; 
-       
-      }
-    };
-    
-// Create Dino Compare Method 3
-Dino.prototype.weightCompare = function (human){
-  const humanWeight = human.weight;
-  const dinoWeight = this.weight;
-  const difference = Math.abs(dinoWeight - humanWeight); 
-  
 
-if (humanWeight < dinoWeight) {
-  return `The ${this.species} weighs ${difference} lbs more then ${human.name}.`;
- 
-  } else if (dinoWeight < humanWeight) {
-  return `The ${this.species} weighs  ${difference} lbs less then ${human.name}.`;  
+// Create Dino Compare Method 2
+Dino.prototype.heightCompare = function(human) {
+  const humanHeight = human.height;
+  const dinoHeight = this.height;
+  const difference = Math.abs(dinoHeight - humanHeight);
+
+  if (humanHeight > dinoHeight) {
+      return `The ${this.species} is  ${difference} inches smaller then ${human.name}.`;
+
+  } else if (dinoHeight > humanHeight) {
+      return `The ${this.species} is  ${difference} inches taller then ${human.name}.`;
   } else {
-   return `the ${this.species} weighs the same as ${human.name}`; 
-   
+      return `the ${this.species} height is the same as ${human.name}`;
+
   }
 };
 
-  // https://knowledge.udacity.com/questions/542357
-    
-// Create Dino Objects
-//add a randomFact method in the Dino constructor 
-Dino.prototype.randomInfo = function (item) {
-  const info = ['weight','height', 'diet', 'fact'];
-  const randomNumber = Math.floor(Math.random() * (info.length -1));
-  if (randomNumber < 3) {
-    return this[info[randomNumber] + 'Compare'](human);
+// Create Dino Compare Method 3
+Dino.prototype.weightCompare = function(human) {
+  const humanWeight = human.weight;
+  const dinoWeight = this.weight;
+  const difference = Math.abs(dinoWeight - humanWeight);
+
+
+  if (humanWeight < dinoWeight) {
+
+      return `The ${this.species} weighs ${difference} lbs more then ${human.name}.`;
+
+  } else if (dinoWeight < humanWeight) {
+      return `The ${this.species} weighs  ${difference} lbs less then ${human.name}.`;
+  } else {
+
+      return `the ${this.species} weighs the same as ${human.name}`;
+
   }
-  
+};
+
+
+//add a randomFact method in the Dino constructor 
+Dino.prototype.randomInfo = function(human) {
+  const info = ['weight', 'height', 'diet', 'fact'];
+  const randomNumber = Math.floor(Math.random() * (info.length - 1));
+  if (randomNumber < 3) {
+      return this[info[randomNumber] + 'Compare'](human);
+  }
+
   return this[info[randomNumber]];
-  //console.log(item);
+
 };
 
 
 //const human = new Human();
-function getHumanData(){
- 
-  human={};
+function getHumanData() {
+
+  human = {};
   human.name = document.getElementById('name').value;
-  human.height = (document.getElementById('feet').value * 12 ) + document.getElementById('inches').value;
+  human.height = (document.getElementById('feet').value * 12) + document.getElementById('inches').value;
   human.weight = document.getElementById('weight').value;
   human.diet = document.getElementById('diet').value;
 
-  console.log(human);
   return human
 };
 
-  // Use IIFE to get human data from form   
-  const getDinos = async () => {
-    const fetchedData = await fetch("./dino.json");
-    const data = await fetchedData.json();
-    return data;
-  };
-  const button = document.getElementById("btn");
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-    const createDinos = async () => {
+// Create Dino Objects 
+const getDinos = async () => {
+  const fetchedData = await fetch("./dino.json");
+  const data = await fetchedData.json();
+
+  return data;
+};
+const button = document.getElementById("btn");
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  const createDinos = async () => {
       const dinos = await getDinos();
       const human = getHumanData();
       const dinoList = dinos.Dinos.map((dino) => {
-        return new Dino(
-          dino.species,
-          dino.height,
-          dino.weight,
-          dino.diet,
-          dino.where,
-          dino.when,
-          dino.fact
-        );
+
+          return new Dino(
+              dino.species,
+              dino.height,
+              dino.weight,
+              dino.diet,
+              dino.where,
+              dino.when,
+              dino.fact
+          );
       });
       dinoList.splice(4, 0, human);
       dinoList.forEach((dino) => {
-        generateUi(dino, human);
+          generateUi(dino, human);
       });
-      
+
       removeForm();
-    };
-    createDinos();
-  });
-  
+  };
+  createDinos();
+});
 
 
 
 
 // Generate Tiles for each Dino in Array
 
-const generateUi=(dino)=>{
-    const grid = document.querySelector('#grid');
-    const head=document.createElement('h3');
-    const image=document.createElement('img');
-    const fact=document.createElement('p');
-    
-    // If dino has a name, it is a human and we want to display it.
-    head.textContent= dino.name || dino.species;
-    // Now, if dino does not have species, it is a human.
-    image.src=`./images/${dino.species || "human"}.png`;
-    // Only show fact if the dino has one.
-    if (dino.fact) {
-      fact.textContent=dino.fact;
-    }
-  
-    if (dino.fact) {
+const generateUi = (dino) => {
+  const grid = document.querySelector('#grid');
+  const head = document.createElement('h3');
+  const image = document.createElement('img');
+  const fact = document.createElement('p');
+
+
+  head.textContent = dino.name || dino.species;
+
+  image.src = `./images/${dino.species || "human"}.png`;
+
+  //if (dino.fact) {
+   //   fact.textContent = dino.fact;
+  //}
+
+  if (dino.fact) {
       fact.textContent = dino.randomInfo(human);
-    }
-   
-   if (dino.species === "Pigeon") {
-     fact.textContent = "All birds are Dinosaurs"; }
+  }
 
-    
+  if (dino.species === "Pigeon") {
+      fact.textContent = "All birds are Dinosaurs";
+  }
+
+
   // Add tiles to DOM
-    const newTile = document.createElement('div');
-    newTile.className = 'grid-item';
-    
-    newTile.appendChild(head);
-    newTile.appendChild(image);
-    newTile.appendChild(fact);
-    grid.appendChild(newTile);
+  const newTile = document.createElement('div');
+  newTile.className = 'grid-item';
 
-  };
-    // Remove form from screen
+  newTile.appendChild(head);
+  newTile.appendChild(image);
+  newTile.appendChild(fact);
+  grid.appendChild(newTile);
+
+};
+// Remove form from screen
 dinoCompare = document.getElementById('dino-compare');
-   function removeForm() {
+
+function removeForm() {
   dinoCompare.style.display = "none";
 };
